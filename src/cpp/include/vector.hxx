f7997ddc739731e3aa6b6dd846fd0ab6e228c841
Salvador Guzman <salvador.guzman@logicmonitor.com>
2015-12-21 23:42:59 -0800

    simple print definition for vector

--- src/cpp/include/vector.hxx
+++ src/cpp/include/vector.hxx
@@ -3,23 +3,12 @@
 #include <vector>
 #include <ostream>
 
+#include "seq.hxx"
+
 using std::vector;
 using std::ostream;
 
-static constexpr const auto vecDelimBegin = '[';
-static constexpr const auto vecDelimEnd = ']';
-static constexpr const auto vecDelimSep = ',';
-
 template <typename T>
-static inline ostream& operator<<(ostream& os, const vector<T>& vec) noexcept {
-  os << vecDelimBegin;
-  if (vec.size() > 0) {
-    os << vec.front();
-
-    for (auto &&it = vec.cbegin() + 1; it < vec.cend(); ++it) {
-      os << vecDelimSep << *it;
-    }
-  }
-
-  return os << vecDelimEnd;
+inline static ostream& operator<<(ostream& os, conref<vector<T>> vec) noexcept {
+  return seq::operator<<(os, vec);
 }
