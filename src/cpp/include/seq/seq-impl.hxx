0fff320170297cf1a85502bbab7affa1e2c5fe81
Salvador Guzman <salvador.guzman@logicmonitor.com>
2015-12-22 20:00:28 -0800

    added seq impl for array

--- src/cpp/include/seq/seq-impl.hxx
+++ src/cpp/include/seq/seq-impl.hxx
@@ -1,11 +1,10 @@
 #pragma once
 
 #include <ostream>
-#include <vector>
+
+#include "../util/def.hxx"
 
 using std::ostream;
-using std::vector;
-
 
 namespace seq {
   static constexpr const auto delimBegin = '[';
@@ -14,6 +13,20 @@ namespace seq {
 
   template <typename T, template <class> typename Seq>
   static inline ostream& operator<<(ostream& os, conref<Seq<T>> vec) noexcept {
+    os << delimBegin;
+    if (vec.size() > 0) {
+      os << vec.front();
+
+      for (auto &&it = vec.cbegin() + 1; it < vec.cend(); ++it) {
+        os << delimSep << *it;
+      }
+    }
+
+    return os << delimEnd;
+  }
+
+  template <typename T, template <class, size_t n> typename Seq, size_t n>
+  static inline ostream& operator<<(ostream& os, conref<Seq<T,n>> vec) noexcept {
     os << delimBegin;
     if (vec.size() > 0) {
       os << vec.front();
