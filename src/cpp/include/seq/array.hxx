a59caca85b98a1dada2173ae46fc579b4e860ce3
Salvador Guzman <salvador.guzman@logicmonitor.com>
2015-12-25 11:28:15 -0800

    print array

--- src/cpp/include/seq/array.hxx
+++ src/cpp/include/seq/array.hxx
@@ -8,8 +8,21 @@
 using std::array;
 using std::ostream;
 
+// Create partial template that pivots on the type of array - not size
+template <size_t n>
+struct seqT {
+    template <typename T>
+    using arrT = array<T,n>;
+
+    template <typename A>
+    inline static void print(ostream& os, conref<arrT<A>> arr) noexcept {
+      seq::seqPrint<A,arrT>::print(os, arr);
+    }
+};
+
 // Overload print function for vector
 template <typename T, size_t n>
 inline static ostream& operator<<(ostream& os, conref<array<T,n>> arr) noexcept {
-  return seq::operator<< <T,array>(os, arr);
+  seqT<n>::print(os, arr);
+  return os;
 }
